a9af8d0387d84368122c17cad69ee78a9d741d06
tinyfrog <tinyfrog12@hanmail.net>
2019-12-10 23:14:45 +0900

    Update loss_function.py

--- ch4/loss_function.py
+++ ch4/loss_function.py
@@ -2,5 +2,8 @@ def mean_squared_error(y, t):
   return 0.5 * np.sum((y-t)**2)
   
 def cross_entropy_error(y, t):
-  delta = 1e-7
-  return -np.sum(t * np.log(y + delta))
+  if y.ndim == 1:
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+  batch_size = y.shape[0]
+  return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
