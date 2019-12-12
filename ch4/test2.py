4759c9ba00a08c91ec016af0f4358447fb0c7ee5
tinyfrog <tinyfrog12@hanmail.net>
2019-12-12 23:29:11 +0900

    Add function differential test

--- ch4/test2.py
+++ ch4/test2.py
@@ -7,7 +7,16 @@ def function_tmp1(x0):
 def function_tmp2(x1):
     return 3.0**2.0 + x1*x1
 
+import numpy as np
+import matplotlib.pylab as plt
 from ch4.loss_function import numerical_diff
 
 print(numerical_diff(function_tmp1, 3.0))
 print(numerical_diff(function_tmp2, 4.0))
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_tmp1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x, y)
+plt.show()
