025b7a0f61b0e453156a4e2a84c4ad3f5a3abe76
tinyfrog <tinyfrog12@hanmail.net>
2019-12-29 20:38:00 +0900

    modify ch6.SGD

--- ch6/SGD.py
+++ ch6/SGD.py
@@ -8,6 +8,30 @@ class SGD:
         for key in params.keys():
             params[key] -= self.lr * grads[key]
 
+class RMSprop:
+    """
+    RMSprop
+        Adagrad와 달리 기울기를 단순 누적하지 않고
+        지수 가중 이동 평균(Exponentially weighted moving average)를 사용
+        최신 기울기들을 더 크게 반영함
+    """
+
+    def __init__(self, lr=0.01, decay_rate = 0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1-self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
 class Momentum:
     def __init__(self, lr=0.01, momentum=0.9):
         self.lr = lr
