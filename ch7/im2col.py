7b27f076372d35b45c7357ba7cea8d594ace0471
tinyfrog <tinyfrog12@hanmail.net>
2020-01-03 23:09:05 +0900

    modify some description

--- ch7/im2col.py
+++ ch7/im2col.py
@@ -3,7 +3,7 @@ import numpy as np
 sys.path.append(os.pardir)
 from common.util import im2col
 
-x1 = np.random.rand(1, 3, 7, 7) # num of data, channel, H, W
+x1 = np.random.rand(1, 3, 7, 7) # number of data, number of channel, H, W
 col1 = im2col(x1, 5, 5, stride=1, pad=0)
 print(col1.shape) # (9, 75)
 
