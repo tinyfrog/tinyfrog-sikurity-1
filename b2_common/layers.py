e06f10ebbe4adcb061b03eef1b6e71c03071bb54
tinyfrog <tinyfrog12@hanmail.net>
2020-01-09 22:59:21 +0900

    finish 1.3.5

--- b2_common/layers.py
+++ b2_common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from b2_common.function import softmax, cross_entropy_error
 
 class MatMul:
     def __init__(self, W):
@@ -27,3 +28,77 @@ class MatMul:
         deep copy를 함
         '''
         return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        W, b = self.params
+        out = np.matmul(x, W) + b
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None # softmax output
+        self.t = None # answer label
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        # if answer label is one-hot vector then convert to answer index
+        self.loss = cross_entropy_error(np.c_[1 - self.y, self.y], self.t)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) * dout / batch_size
+        return dx
+
+
+
+
